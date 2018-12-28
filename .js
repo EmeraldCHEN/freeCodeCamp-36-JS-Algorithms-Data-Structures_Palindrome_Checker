7097e51243e0c4c9d6cc5d806a0d4be78a55ca12
EmeraldNZ <ahyuancy@gmail.com>
2018-12-28 22:06:22 +1300

    Create .js

---
+++ .js
@@ -0,0 +1,18 @@
+function palindrome(str) {
+    let arr = str.toLowerCase().split("");
+ 
+    arr = arr.filter(x => /[a-zA-Z0-9]/.test(x));
+    let count = 0;
+    for(let i = 0; i <= (arr.length - i - 1); i++){
+        if(arr[i] == arr[arr.length - i - 1]){
+          count++;
+           // return true; 
+        }                   
+    }
+    if(count == arr.length/2 || count == (arr.length + 1)/2 ){
+      return true;
+    }else{
+      return false;
+    }  
+}
+palindrome("eye");
